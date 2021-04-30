b48912da4dc8ed31585f10927d96dc87125ed0ca
Calvin Hutcheon <chutcheon@mica.edu>
2021-04-30 13:37:23 -0700

    testing content persistence

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,33 @@
-function aboutPage() {
+import Error from "next/error";
+import { useRouter } from "next/router";
+import { getClient, usePreviewSubscription } from "../utils/sanity";
+
+const query = `//groq
+  *[_type == "about" ]
+`;
+
+function aboutPage(props) {
+    const { aboutData, preview } = props;
+    const router = useRouter();
+    const { title, description } = aboutData;
+
     return(
         <>
-            <h1>About</h1>
-            <h1>Faq</h1>
+            <h1>{title}</h1>
+            <h1>{description}</h1>
         </>
     )
 }
+
+export async function getStaticProps({ params = {}, preview = false }) {
+    const aboutData = await getClient(preview).fetch(query);
+  
+    return {
+      props: {
+        preview,
+        aboutData,
+      },
+    };
+  }
 
 export default aboutPage;
